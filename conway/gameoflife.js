520894d4a305649abde92387b2a9ec098fc7f110
Zeroth96 <zeroth96@hotmail.com>
2015-10-18 19:54:06 +1100

    fixed cursor changing on drag

--- conway/gameoflife.js
+++ conway/gameoflife.js
@@ -191,10 +191,12 @@ $(document).ready(function(){
 	
 	//pixel draw method
 	$(canvas).mousedown(function(event) {
+		event.preventDefault();
 		isDragging = true;
 		addCellAtCursor(event);
 	})
 	.mousemove(function(event) {
+		event.preventDefault();
 		if (isDragging) {
 			addCellAtCursor(event);
 		}
